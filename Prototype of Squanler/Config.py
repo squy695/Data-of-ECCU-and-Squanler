06f490a0907cba73abbad46b2fdcd16f0e363723
Squanchx695 <115967215+squy695@users.noreply.github.com>
2025-08-22 16:48:21 +0800

    Update Config.py

--- Prototype of Squanler/Config.py
+++ Prototype of Squanler/Config.py
@@ -6,41 +6,49 @@ def getNowTime():
 class Config:
     def __init__(self):
         
-        # for now, we only consider these three status codes
+        # You can enhance the cycles configuration by adding additional status codes that may be relevant to your application's entry interfaces.
+        # For now, we only consider these three status codes
         self.cycles = ['2..', '302', '5..']
         
+        # The namespace of your application.
         self.namespace = 'hipster'
+        # The namespace of your cluster's entry point (typically istio-ingressgateway).
         self.ingress_namespace = 'istio-system'
+        # The name of your cluster's entry point.
         self.ingress = 'istio-ingressgateway'
         
+        # The call chain relationships between your application's entry interfaces (parent -> child).
         self.redict_chain = [
             ['/setCurrency && POST', '/ && GET'],
         ]
 
-        self.SLO = 200
+        # The target end-to-end latency.
+        self.Target = 200
+        # The execution interval for Squanler.
         self.scale_interval = 30
+        # The CPU request for the service.
         self.request = 150
 
+        # The services.
         self.svcs = ['frontend','adservice','cartservice','checkoutservice','currencyservice','emailservice','paymentservice','productcatalogservice','recommendationservice','shippingservice']
         
-        # your ISH
+        # your ISH, refer to the appendix for collection.
         self.interfaces = {
-            "/ && GET": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], self.SLO],
-            "/product/{id} && GET": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], self.SLO],
-            '/cart && GET': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], self.SLO],
-            '/cart/checkout && POST': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], self.SLO],
-            '/setCurrency && POST': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], self.SLO],
+            "/ && GET": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], self.Target],
+            "/product/{id} && GET": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], self.Target],
+            '/cart && GET': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], self.Target],
+            '/cart/checkout && POST': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], self.Target],
+            '/setCurrency && POST': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], self.Target],
         }
 
         self.max_pod = 100
         self.min_pod = 1
-
-        self.k8s_config = 'admin.conf'
 
         self.duration = 1 * 60 * 60
         self.start = getNowTime()
         self.end = self.start + self.duration
         self.step = 5
 
+        # The address of your Prometheus instance.
         self.prom_range_url = "http://XXX.XXX.XX.XXX:XXXXX//api/v1/query_range"
         self.prom_no_range_url = "http://XXX.XXX.XX.XXX:XXXXX//api/v1/query"
